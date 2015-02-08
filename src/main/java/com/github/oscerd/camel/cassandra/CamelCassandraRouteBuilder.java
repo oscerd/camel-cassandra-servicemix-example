3b48da254df5d34d1db5360fac64f3371f66126d
ancosen <ancosen@gmail.com>
2015-02-08 11:19:18 +0100

    Logging info added

--- src/main/java/com/github/oscerd/camel/cassandra/CamelCassandraRouteBuilder.java
+++ src/main/java/com/github/oscerd/camel/cassandra/CamelCassandraRouteBuilder.java
@@ -10,7 +10,6 @@ import java.util.UUID;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.main.Main;
 
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
@@ -28,22 +27,50 @@ public class CamelCassandraRouteBuilder extends RouteBuilder {
     	List<String> collAddr = new ArrayList<String>();
     	collAddr.add(addr);
     	
+        Set<String> tags = new HashSet<String>();
+        tags.add("1993");
+        tags.add("Alternative Metal");
+        HashMap<String, Object> insert = new HashMap<String, Object>();
+        insert.put("id", UUID.randomUUID());
+        insert.put("album", "Undertow");
+        insert.put("title", "Bottom");
+        insert.put("artist", "Tool");
+        insert.put("tags", tags);
+    	
         from ("timer://timer?fixedRate=true&period=10000&repeatCount=1")
         .setHeader(CassandraConstants.CASSANDRA_CONTACT_POINTS, constant(collAddr))
+        .to("log:Querying Cassandra")
         .to("cassandra:cassandraConnection?keyspace=simplex&table=songs&operation=selectAll")
         .process(new Processor() {
 			
 			@Override
 			public void process(Exchange ex) throws Exception {
 				ResultSet resultSet = (ResultSet) ex.getIn().getBody();
-				StringBuilder stringBuilder = new StringBuilder();
 				for (Row row : (ResultSet) resultSet) {
 					log.info("Id: " + row.getUUID("id") + " - Album: " + row.getString("album") + " - Title: " + row.getString("title")); 
 		        }
 			}
 		
 		})
-		.stop();
+		.setBody(constant(""))
+		.setHeader(CassandraConstants.CASSANDRA_INSERT_OBJECT, constant(insert))
+		.to("log:Inserting the object")
+		.to("cassandra:cassandraConnection?keyspace=simplex&table=songs&operation=insert")
+		.removeHeader(CassandraConstants.CASSANDRA_INSERT_OBJECT)
+		.setBody(constant(""))
+		.to("log:Querying Cassandra")
+		.to("cassandra:cassandraConnection?keyspace=simplex&table=songs&operation=selectAll")
+        .process(new Processor() {
+			
+			@Override
+			public void process(Exchange ex) throws Exception {
+				ResultSet resultSet = (ResultSet) ex.getIn().getBody();
+				for (Row row : (ResultSet) resultSet) {
+					log.info("Id: " + row.getUUID("id") + " - Album: " + row.getString("album") + " - Title: " + row.getString("title")); 
+		        }
+				
+			}
+		});
     }
 
 }
